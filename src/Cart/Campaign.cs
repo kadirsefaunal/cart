0878299f24080a8c1322630506f6404ebdd908cc
kadirsefaunal <kadirsefau@gmail.com>
2019-12-22 16:20:06 +0300

    Add Campaign class

---
+++ src/Cart/Campaign.cs
@@ -0,0 +1,25 @@
+using Cart.Enums;
+
+namespace Cart
+{
+    public class Campaign
+    {
+        private Category _category;
+        private double _amount;
+        private int _quantity;
+        private DiscountType _discountType;
+
+        public Campaign(Category category, double amount, int quantity, DiscountType discountType)
+        {
+            this._category = category;
+            this._amount = amount;
+            this._quantity = quantity;
+            this._discountType = discountType;
+        }
+
+        public Category Category => _category;
+        public double Amount => _amount;
+        public int Quantity => _quantity;
+        public DiscountType DiscountType => _discountType;
+    }
+}
