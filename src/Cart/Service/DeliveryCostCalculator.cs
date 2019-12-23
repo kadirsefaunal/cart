be6d9274dad8c0217e0679a363adb91c57464d7b
kadirsefaunal <kadirsefau@gmail.com>
2019-12-23 22:51:55 +0300

    Add delivery amount calculation

--- src/Cart/Service/DeliveryCostCalculator.cs
+++ src/Cart/Service/DeliveryCostCalculator.cs
@@ -1,9 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+using Cart.Core;
+
 namespace Cart.Service
 {
     public class DeliveryCostCalculator
     {
-        public double CostPerDelivery { get; set; }
-        public double CostPerProduct { get; set; }
-        public double FixedCost { get; set; }
+        private readonly double _costPerDelivery;
+        private readonly double _costPerProduct;
+        private readonly double _fixedCost;
+
+        public DeliveryCostCalculator(double costPerDelivery, double costPerProduct, double fixedCost)
+        {
+            _costPerDelivery = costPerDelivery;
+            _costPerProduct = costPerProduct;
+            _fixedCost = fixedCost;
+        }
+
+        /// <summary>
+        ///     Calculates the delivery amount of given cart.
+        /// </summary>
+        public double CalculateFor(ShoppingCart cart)
+        {
+            var numberOfDeliveries = GetNumberOfDeliveries(cart.CartItems);
+            var numberOfProduct = GetNumberOfProduct(cart.CartItems);
+
+            return _costPerDelivery * numberOfDeliveries + _costPerProduct * numberOfProduct + _fixedCost;
+        }
+
+        private int GetNumberOfDeliveries(List<CartItem> cartItems)
+        {
+            return cartItems.Select(i => i.Product.Category.Title).Distinct().Count();
+        }
+
+        private int GetNumberOfProduct(List<CartItem> cartItems)
+        {
+            return cartItems.Count();
+        }
     }
 }
--- tests/Cart.Tests/DeliveryCostCalculatorTests.cs
+++ tests/Cart.Tests/DeliveryCostCalculatorTests.cs
@@ -1,7 +1,37 @@
+using Cart.Core;
+using Cart.Service;
+using Xunit;
+
 namespace Cart.Tests
 {
     public class DeliveryCostCalculatorTests
     {
-        
+        private const double FixedCost = 2.99;
+            
+        private ShoppingCart getShoppingCart()
+        {
+            var category = new Category("test");
+
+            var product1 = new Product("product1", 300, category);
+            var product2 = new Product("product2", 50, category);
+
+            var shoppingCart = new ShoppingCart();
+            shoppingCart.AddItem(product1, 2);
+            shoppingCart.AddItem(product2, 6);
+            shoppingCart.AddItem(product1, 1);
+
+            return shoppingCart;
+        }
+
+        [Fact]
+        public void TestCalculateDelivery()
+        {
+            var cart = getShoppingCart();
+            
+            var deliveryCostCalculator = new DeliveryCostCalculator(5, 0.7, FixedCost);
+            var deliveryAmount = deliveryCostCalculator.CalculateFor(cart);
+            
+            Assert.Equal(9.39, deliveryAmount);
+        }
     }
 }
